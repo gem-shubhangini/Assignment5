d684d02c6dc3348fe78d0c597fd67dec0ab75cff
SHUBHANGINI SHARMA <SHUBHANGINISHARMA67@gmail.com>
2022-08-20 19:18:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import UserForm from './Form';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TableData from './Table';
 function App() {
   return (
     <div className="App">
   
-      <UserForm></UserForm>
+  <BrowserRouter>
+      <Routes>
+        <Route path="/users/view" element={<TableData/>}>
+          <Route index path="/users/create" element={<UserForm />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
       
     </div>
   );
